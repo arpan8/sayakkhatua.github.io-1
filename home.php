7c7605f62307c8100113231302f036d85bcfb689
Sayak Khatua <sayakkhatua@yahoo.in>
2019-08-13 19:12:29 +0530

    kk

---
+++ home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Home</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+<?php require "header.php"; ?>
+<div class="container">
+<div class="about">
+ <img src="KhatuaSayak.jpg" class="img-circle" width="200",height="200">&nbsp;&nbsp;&nbsp;&nbsp;
+  <p align="justify"> Hi. I’m Sayak Khatua. I am research associate at the <a href="https://www.3ieimpact.org/">International Initiative for Impact Evaluation (3ie)</a> I am an economist with a keen interest in public policy analysis, impact evaluations, open science and replication research. I currently work on the research transparency initiative at 3ie and support the WASH thematic window.</p>
+  </div>
+</div>
+<?php require "footer.php"; ?>
+</body>
+</html>
